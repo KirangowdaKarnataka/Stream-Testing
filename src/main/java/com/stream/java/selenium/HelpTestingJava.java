9f6c4457b97a90a499847e52520dfcf29ed180bd
kirangowda <kirangowda456qwe@gmail.com>
2024-07-08 19:36:55 +0530

    updates text

---
+++ src/main/java/com/stream/java/selenium/HelpTestingJava.java
@@ -0,0 +1,12 @@
+package com.stream.java.selenium;
+
+public class HelpTestingJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("good morning hello worls");
+
+	}
+
+}
